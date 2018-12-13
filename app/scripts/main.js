ad274131ed1bbb4565d1ebbc720b0b3b11d44a21
langziguilai <langziguilai2010@gmail.com>
2018-12-13 22:27:49 +0800

    commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -49,47 +49,111 @@
   }
 })();
 $(document).ready(function () {
-  function layout(){
-      // 加载瀑布流
-      let windowWidth=$(window).width()
-      if(windowWidth<=576){
-        $('.cards').css({"width":"488px"})
-        $('.cards').masonry({
-          // set itemSelector so .grid-sizer is not used in layout
-          itemSelector: '.card',
-          // use element for option
-          columnWidth: '.card',
-          percentPosition: true,
-          gutter: 16,
-          resize: false
-        })
-      }else if(windowWidth<=739){
-        $('.cards').css({"width":"488px"})
-        $('.cards').masonry({
-          // set itemSelector so .grid-sizer is not used in layout
-          itemSelector: '.card',
-          // use element for option
-          columnWidth: '.card',
-          percentPosition: false,
-          gutter: 16,
-          resize: false
-        })
-      }else{
-        $('.cards').css({"width":"100%"})
-        $('.cards').masonry({
-          // set itemSelector so .grid-sizer is not used in layout
-          itemSelector: '.card',
-          // use element for option
-          columnWidth: 236,
-          percentPosition: true,
-          gutter: 16,
-          resize: false
-        })
-      }
-      //$('body').css("min-width",$(window).width()+"px")
+  function layout() {
+    // 加载瀑布流
+    let windowWidth = $(window).width()
+    if (windowWidth <= 576) {
+      $('.cards').css({
+        "width": "488px"
+      })
+      $('.cards').masonry({
+        // set itemSelector so .grid-sizer is not used in layout
+        itemSelector: '.card',
+        // use element for option
+        columnWidth: '.card',
+        percentPosition: true,
+        gutter: 16,
+        resize: false
+      })
+    } else if (windowWidth <= 739) {
+      $('.cards').css({
+        "width": "488px"
+      })
+      $('.cards').masonry({
+        // set itemSelector so .grid-sizer is not used in layout
+        itemSelector: '.card',
+        // use element for option
+        columnWidth: '.card',
+        percentPosition: false,
+        gutter: 16,
+        resize: false
+      })
+    } else {
+      $('.cards').css({
+        "width": "100%"
+      })
+      $('.cards').masonry({
+        // set itemSelector so .grid-sizer is not used in layout
+        itemSelector: '.card',
+        // use element for option
+        columnWidth: 236,
+        percentPosition: true,
+        gutter: 16,
+        resize: false
+      })
+    }
+    //$('body').css("min-width",$(window).width()+"px")
   }
   layout()
-  $(window).resize(function(){
+  $(window).resize(function () {
     layout()
   })
+
+  function initCommentEditor() {
+    if (!window.wangEditor) {
+      return
+    }
+    var E = window.wangEditor
+    var editor = new E('#comment_editor_header', '#comment_editor_body') // 两个参数也可以传入 elem 对象，class 选择器
+    // 自定义菜单配置
+    editor.customConfig.menus = [
+      'head',
+      'bold',
+      'italic',
+      'underline',
+      'justify', // 对齐方式
+      'quote', // 引用
+      'emoticon', // 表情
+    ]
+    editor.create()
+    //点击提交
+    $("#new_comment").click(function () {
+      //如果已经登录
+      if (window.user && window.blogId) {
+        let commentText = editor.txt.text()
+        if (commentText.length > 5) {
+          $.ajax({
+            type: "POST",
+            url: "/api/v1/comments",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({blog_id:window.blogId,content:commentText}),
+            dataType: "json",
+            success: function (comment) {
+              //TODO:add to comment list
+              //隐藏
+              $("#comment_editor").css("display","none")
+            },
+            error: function (message) {
+              //提示错误
+              swal({
+                title: '错误',
+                text: message.error,
+                icon: 'error',
+                button: '确定',
+              })
+            }
+          });
+        } else {
+              //提示错误
+              swal({
+                title: '警告',
+                text: '请输入至少5个字符！！！',
+                icon: 'warning',
+                button: '确定',
+              })
+        }
+      }
+    })
+  }
+  initCommentEditor()
 });
